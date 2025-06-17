893ea98f8684b90a197e88534464a7ddfc91d3dc
Michael Sheckler <tincan2012@yahoo.com>
2025-06-17 14:08:51 -0500

    [UPDATES] Giving the app singularity.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,41 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Runtime.InteropServices;
 using System.Windows;
 
 namespace StarforgeLauncher
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private const string MutexName = "StarforgeLauncher";
+        private Mutex? mutex;
+        [DllImport("shell32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int SetCurrentProcessExplicitAppUserModelID(string AppID);
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SetCurrentProcessExplicitAppUserModelID(MutexName);
+
+            bool createdNew;
+            mutex = new Mutex(true, MutexName, out createdNew);
+
+            if (!createdNew)
+            {
+                System.Windows.MessageBox.Show("Another instance of the application is already running.");
+                Environment.Exit(0);
+            }
+
+            base.OnStartup(e);
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (mutex != null)
+            {
+                mutex.ReleaseMutex();
+                mutex.Dispose();
+            }
+            base.OnExit(e);
+        }
     }
 
 }
