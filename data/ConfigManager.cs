4e099e3d2e6a3d3ea06a7722e941b4dafb40f87d
Michael Sheckler <tincan2012@yahoo.com>
2026-03-26 18:17:02 -0500

    [UPDATE] Version 1.02.24 Launchpad and Version 0.1.5 Launcher Release.

--- data/ConfigManager.cs
+++ data/ConfigManager.cs
@@ -15,7 +15,7 @@ namespace StarforgeLauncher.data
 
     public static class ConfigFileVariables
     {
-        public static string launcherVersion = "0.1.5";
+        public static string launcherVersion = "0.1.6";
         public static string launchPadVersion = "0.0.0";
         public static string InstallDirectory = Path.Combine(LauncherClientVariables.LaunchPadDirectory, "Starforge-Client");
         public static bool isInstalled = false;
